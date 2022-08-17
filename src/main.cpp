fc3da1d3906ff98e385ace7d9bcdb18353be9077
fenix31415 <dimkakirov43@mail.ru>
2022-08-17 04:17:28 +0300

    release

--- src/main.cpp
+++ src/main.cpp
@@ -72,77 +72,194 @@ public:
 		}
 	}
 
-	static inline iSetting Formid{ "General"s, "FormID"s, 0x800 };
-	static inline sSetting EspName{ "General"s, "Esp"s, "ConfigurableCastTime.esp" };
 	static inline bSetting EnableNPC{ "General"s, "EnableNPC"s, false };
 	static inline bSetting EnablePlayer{ "General"s, "EnablePlayer"s, true };
+
+	//static inline iSetting Formid{ "General"s, "FormID"s, 0x800 };
+	//static inline sSetting EspName{ "General"s, "Esp"s, "ConfigurableCastTime.esp" };
 };
 
-struct DataHandler
-{
-	static inline RE::TESGlobal* var = nullptr;
+//struct DataHandler
+//{
+//	static inline RE::TESGlobal* var = nullptr;
+//
+//	static void init() {
+//		var = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESGlobal>(static_cast<uint32_t>(*Settings::Formid), *Settings::EspName);
+//	}
+//};
 
-	static void init() {
-		var = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESGlobal>(static_cast<uint32_t>(*Settings::Formid), *Settings::EspName);
-	}
-};
+float get_skill_pers(RE::Actor* a, RE::SpellItem* spel) {
+	const float MAX_SKILL = 100.0f;
+	auto av = spel->GetAssociatedSkill();
+	return av != RE::ActorValue::kNone ? a->GetActorValue(av) / MAX_SKILL : -1.0f;
+}
 
-float get_cast_time(float origin, RE::MagicCaster* _a)
+float get_cast_time_Impl([[maybe_unused]] float origin, [[maybe_unused]] RE::Actor* caster, [[maybe_unused]] RE::SpellItem* spel)
 {
-	auto a = _a->GetCasterAsActor();
-	if (!a)
-		return origin;
+	//return origin * DataHandler::var->value;
 	
+	//37.8255 x^(5)-74.8895 x^(4)+43.3082 x^(3)-7.65238 x^(2)+4.82019 x-0.411975
+	
+	float x = get_skill_pers(caster, spel);
+	if (x < 0.0f)
+		return origin;
+
+	return origin / (37.8255f * x * x * x * x * x - 74.8895f * x * x * x * x + 43.3082f * x * x * x - 7.65238f * x * x + 4.82019f * x - 0.411975f);
+}
+
+bool is_playable_spel(RE::SpellItem* spel) {
+	using ST = RE::MagicSystem::SpellType;
+	using AV = RE::ActorValue;
+
+	auto type = spel->GetSpellType();
+	if (type == ST::kStaffEnchantment || type == ST::kScroll || type == ST::kSpell || type == ST::kLeveledSpell) {
+		auto av = spel->GetAssociatedSkill();
+		return av == AV::kAlteration || av == AV::kConjuration || av == AV::kDestruction || av == AV::kIllusion ||
+		       av == AV::kRestoration;
+	}
+
+	return false;
+}
+
+float get_cast_time(float origin, RE::Actor* a, RE::SpellItem* spel)
+{
 	bool isPlayer = a->IsPlayerRef();
-	if (isPlayer && *Settings::EnablePlayer || !isPlayer && *Settings::EnableNPC) {
-		//static auto varr = RE::TESDataHandler::GetSingleton()->LookupForm<RE::TESGlobal>(static_cast<uint32_t>(*Settings::Formid), *Settings::EspName);
-		return origin * DataHandler::var->value;
-		//return origin * varr->value;
+	if (is_playable_spel(spel) && (isPlayer && *Settings::EnablePlayer || !isPlayer && *Settings::EnableNPC)) {
+		return get_cast_time_Impl(origin, a, spel);
 	} else {
 		return origin;
 	}
 }
 
-#include <xbyak\xbyak.h>
-template <size_t BRANCH_TYPE, uint64_t ID, size_t offset = 0, bool call = false>
-void add_trampoline(Xbyak::CodeGenerator* xbyakCode)
+float get_cast_time_caster(float origin, RE::MagicCaster* _a, RE::SpellItem* spel)
 {
-	constexpr REL::ID funcOffset = REL::ID(ID);
-	auto funcAddr = funcOffset.address();
-	auto size = xbyakCode->getSize();
-	auto& trampoline = SKSE::GetTrampoline();
-	auto result = trampoline.allocate(size);
-	std::memcpy(result, xbyakCode->getCode(), size);
-	if constexpr (!call)
-		trampoline.write_branch<BRANCH_TYPE>(funcAddr + offset, (std::uintptr_t)result);
-	else
-		trampoline.write_call<BRANCH_TYPE>(funcAddr + offset, (std::uintptr_t)result);
+	auto a = _a->GetCasterAsActor();
+	if (!a)
+		return origin;
+	
+	return get_cast_time(origin, a, spel);
 }
 
-void apply_hook() {
-	// mul time to global
-	
-	// SkyrimSE.exe+54F231
-	constexpr REL::ID funcOffset = REL::ID(33651);
-	uintptr_t ret_addr = funcOffset.address() + 0x21;
+float get_cast_time_addone(float origin, RE::Character* a, RE::SpellItem* spel) { return get_cast_time(origin, a, spel) + 1.0f; }
 
-	struct Code : Xbyak::CodeGenerator
+float get_cast_time_spell_caster(RE::SpellItem* spel, RE::MagicCaster* _a)
+{
+	return get_cast_time_caster(spel->GetChargeTime(), _a, spel);
+}
+
+template <bool eq>
+bool MagicItem__is_continious_common(RE::SpellItem* spel, RE::MagicCaster* caster)
+{
+	if (!spel)
+		return false;
+
+	using namespace RE::MagicSystem;
+	auto type = spel->GetSpellType();
+	if (type != SpellType::kSpell && type != SpellType::kLesserPower && type != SpellType::kPoison &&
+		(type != SpellType::kEnchantment && type != SpellType::kStaffEnchantment ||
+			spel->GetCastingType() == CastingType::kConstantEffect || caster->GetCastingSource() == CastingSource::kInstant))
+		return false;
+
+	float charge_time = get_cast_time_spell_caster(spel, caster);
+	if (eq) {
+		if (charge_time == 0.0)
+			return false;
+	} else {
+		if (charge_time != 0.0)
+			return false;
+	}
+
+	return spel->GetCastingType() == CastingType::kFireAndForget;
+}
+
+bool MagicItem__is_continious_v1(RE::SpellItem* spel, RE::MagicCaster* caster)
+{
+	return MagicItem__is_continious_common<false>(spel, caster);
+}
+
+bool MagicItem__is_continious_v2(RE::SpellItem* spel, RE::MagicCaster* caster)
+{
+	return MagicItem__is_continious_common<true>(spel, caster);
+}
+
+void apply_hooks()
+{
+	using FenixUtils::add_trampoline;
+
 	{
-		Code(uintptr_t func_addr, uintptr_t ret_addr)
+		// SkyrimSE.exe+54291A
+		FenixUtils::writebytes<33364, 0x16a>("\x48\x89\xDA\x0F\x1F\x00"sv);  // rbx
+		SKSE::GetTrampoline().write_call<5>(REL::ID(33364).address() + 0x173, uintptr_t(MagicItem__is_continious_v1));
+
+		// SkyrimSE.exe+5425AF
+		FenixUtils::writebytes<33363, 0x13f>("\x48\x89\xFA\x0F\x1F\x00"sv);  // rdi
+		SKSE::GetTrampoline().write_call<5>(REL::ID(33363).address() + 0x148, uintptr_t(MagicItem__is_continious_v1));
+
+		// SkyrimSE.exe+541DA5
+		FenixUtils::writebytes<33359, 0x15>("\x48\x89\xCA\x0F\x1F\x00"sv);  // rcx
+		SKSE::GetTrampoline().write_call<5>(REL::ID(33359).address() + 0x1e, uintptr_t(MagicItem__is_continious_v2));
+
+		// SkyrimSE.exe+542198
+		FenixUtils::writebytes<33362, 0x1d8>("\x48\x89\xDA\x0F\x1F\x00"sv);  // rcx
+		SKSE::GetTrampoline().write_call<5>(REL::ID(33362).address() + 0x1e1, uintptr_t(MagicItem__is_continious_v2));
+	}
+
+	{  // ActorMagicCaster::Update
+		struct Code : Xbyak::CodeGenerator
 		{
-			mov(rdx, rbx);
-			mov(rax, func_addr);
-			call(rax);
+			Code(uintptr_t func_addr, uintptr_t ret_addr)
+			{
+				// rcx == SpellItem*
+				// rbx == MagicCaster*
+				mov(rdx, rbx);
+				mov(rax, func_addr);
+				call(rax);
 
-			// recover
-			mov(rax, ptr[rbx]);
-			mov(rcx, rbx);
+				mov(rdx, ret_addr);
+				jmp(rdx);
+			}
+		} xbyakCode{ uintptr_t(get_cast_time_spell_caster), REL::ID(33362).address() + 0x21c };  // SkyrimSE.exe+5421DC
+		add_trampoline<5, 33362, 0x216>(&xbyakCode);                                             // SkyrimSE.exe+5421D6
+	}
 
-			mov(rdx, ret_addr);
-			jmp(rdx);
-		}
-	} xbyakCode{ uintptr_t(get_cast_time), ret_addr };
-	add_trampoline<5, 33651, 0x1b>(&xbyakCode);  // SkyrimSE.exe+54F22B
+	{  // PlayerCharacter::sub_1407BEED0
+		struct Code : Xbyak::CodeGenerator
+		{
+			Code(uintptr_t func_addr, uintptr_t ret_addr)
+			{
+				// rdi == Character*
+				// rbx == SpellItem*
+				mov(r8, rbx);
+				mov(rdx, rdi);
+				mov(rax, func_addr);
+				call(rax);
+
+				mov(rdx, ret_addr);
+				jmp(rdx);
+			}
+		} xbyakCode{ uintptr_t(get_cast_time_addone), REL::ID(46070).address() + 0x13d };  // SkyrimSE.exe+7BF00D
+		add_trampoline<5, 46070, 0x135>(&xbyakCode);                                       // SkyrimSE.exe+7BF005
+	}
+
+	{  // MagicCaster::SetCastingTimerForCharge
+		struct Code : Xbyak::CodeGenerator
+		{
+			Code(uintptr_t func_addr, uintptr_t ret_addr)
+			{
+				// rcx == SpellItem*
+				// rbx == MagicCaster*
+				mov(rdx, rbx);
+				mov(rax, func_addr);
+				call(rax);
+
+				mov(rdx, ret_addr);
+				jmp(rdx);
+			}
+		} xbyakCode{ uintptr_t(get_cast_time_spell_caster), REL::ID(33651).address() + 0x1b };  // SkyrimSE.exe+54F22B
+		add_trampoline<5, 33651, 0x15>(&xbyakCode);                                             // SkyrimSE.exe+54F225
+	}
+
+	// ignored 140892130 (314 or 314/s, comparing with 0)
 }
 
 static void SKSEMessageHandler(SKSE::MessagingInterface::Message* message)
@@ -150,8 +267,8 @@ static void SKSEMessageHandler(SKSE::MessagingInterface::Message* message)
 	switch (message->type) {
 	case SKSE::MessagingInterface::kDataLoaded:
 		Settings::load();
-		DataHandler::init();
-		apply_hook();
+		//DataHandler::init();
+		apply_hooks();
 
 		break;
 	}
